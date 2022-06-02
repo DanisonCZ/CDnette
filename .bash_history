68f6645df46d5bbe58818c51224904f338b3add5
dan <danisonp1@gmail.com>
2022-06-02 19:39:46 +0200

    login

---
+++ .bash_history
@@ -0,0 +1,17 @@
+git clone https://gitlab.spseplzen.cz/bosakm/travelblog.git
+git config [--global] user.name "dan"
+git init
+git remote add origin https://gitlab.spseplzen.cz/horesovskym/prestizni-preziti.git
+git branch
+git branch main
+git pull
+git add remote origin https://github.com/nette/tutorial-quickstart.git 
+git init
+git add remote origin https://github.com/nette/tutorial-quickstart.git 
+git add origin https://github.com/nette/tutorial-quickstart.git 
+git remote add origin https://github.com/nette/tutorial-quickstart.git 
+git pull
+git clone https://github.com/nette/tutorial-quickstart.git
+git clone https://github.com/nette-examples/cd-collection
+cd cd-collection
+composer install
--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -9,20 +9,88 @@ use Nette\Application\UI\Form;
 
 final class HomepagePresenter extends Nette\Application\UI\Presenter
 {
+
 	private Nette\Database\Explorer $database;
+
 
 	public function __construct(Nette\Database\Explorer $database)
 	{
 		$this->database = $database;
+
+        $paginator = new Nette\Utils\Paginator;
+        $paginator->setPage(1); // číslo aktuální stránky
+        $paginator->setItemsPerPage(30); // počet položek na stránce
+        $paginator->setItemCount(356); // celkový počet položek, je-li znám
 	}
 
-    public function renderDefault(): void
-{
-	$this->template->dbcd = $this->database
-		->table('cd')
-		->order('id DESC')
-		->limit(5);
-}
+    public function startup(): void
+    {
+	    parent::startup();
 
+        if (!$this->getUser()->isLoggedIn()) {
+            $this->redirect('Sign:in');
+        }
+    }
+
+
+    public function renderDefault(): void
+    {
+        $this->template->dbcd = $this->database
+            ->table('cd')
+            ->order('id');
+    }
+
+    protected function createComponentCommentForm(): Form
+    {
+        $form = new Form; // means Nette\Application\UI\Form
+
+        $form->addText('nazevCD', 'Název: ')
+            ->setRequired('Zadejte název CD');
+
+        $form->addInteger('delkaCD', 'Délka: ')
+            ->setRequired('Zadejte délku CD')
+            ->addRule(
+                [HomepagePresenter::class, 'validateOver0'],
+                'Délka musí být vetší než 0'
+            );
+
+        $form->addText('autorCD', 'Autor: ')
+            ->setRequired('Zadejte dautora CD');
+
+        $form->addText('datumCD', 'Datum vydání: ')
+            ->setType('Date')
+            ->setRequired('Zadejte datum vydání CD');
+
+        $form->addSubmit('send', 'Přidat');
+
+        $form->onSuccess[] = [$this, 'formSucceeded'];
+
+        return $form;
+    }
+
+	public static function validateOver0($input): bool
+	{
+		return $input->getValue() > 0;
+	}
+
+
+    public function formSucceeded(\stdClass $data): void
+    {
+        //$postId = $this->getParameter('postId');
+
+        if ( is_int($data->delkaCD) && ($data->delkaCD)>0 )
+        {
+        $this->database->table('cd')->insert([
+            'nazev' => $data->nazevCD,
+            'delka' => $data->delkaCD,
+            'autor' => $data->autorCD,
+            'datum' => $data->datumCD,
+        ]);
+            $this->flashMessage('Záznam přidán', 'success');
+        } else {
+            $this->flashMessage('špatný formát délky');
+        }
+        $this->redirect('this');
+    }
 
 }
